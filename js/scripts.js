b8095428d11699cc4d8d69ba697cc42928949806
pinko <agaslota@wp.pl>
2017-06-05 14:58:12 +0100

    pierwsza choinka

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+function drawTree(i){
+	for (var i=0; i<10; i++) {
+		var star = toString();
+		for (var j=0; j<=i; j++) {
+			var star = star += '*';
+		}
+	console.log(star);
+	}
+}
+console.log(drawTree(6));
+// czy o takie coś chodziło? 
