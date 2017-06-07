71d0cce6c1969e4bcfa356561a9d7b4fb240fa67
pinko <agaslota@wp.pl>
2017-06-07 13:20:22 +0100

    usunieto debuggera

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,8 @@
 function drawTree(x) {
-	//tworzy rzędy:
-	debugger;
 	for (var i=0; i<x; i++) {
 		var gwiazdka = '*';
-		//ilosc w rzedzie
 		for (var j=0; j<i; j++) {
-			gwiazdka = gwiazdka + '*'
-
+			gwiazdka = gwiazdka + '*';
 		}
 		console.log(gwiazdka);
 	}
